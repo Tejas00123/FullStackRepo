11b8aece37d27cf2f67db59ba1d10604624baada
Tejas Gavali <tejasgavali8280@gmail.com>
2024-02-27 21:57:26 +0530

    dev1 -- change1st

--- Proj1/src/main/java/com/nt/service/App1.java
+++ Proj1/src/main/java/com/nt/service/App1.java
@@ -5,6 +5,9 @@ public class App1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ArithmaticOperation obj = new ArithmaticOperation();
+		
+		System.out.println("Addition::"+obj.add(10, 20));
 	}
 
 }
