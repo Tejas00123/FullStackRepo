0676c46fdb4862008a0bfb5f51f024fc7ac9a83d
Tejas Gavali <tejasgavali8280@gmail.com>
2024-02-28 19:51:27 +0530

    dev1--- taks1 debit/credit payment done

--- Proj1/src/main/java/com/nt/Payment/PaymentService.java
+++ Proj1/src/main/java/com/nt/Payment/PaymentService.java
@@ -2,5 +2,14 @@ package com.nt.Payment;
 
 public class PaymentService {
 
+	public String DebicardPayment()
+	{
+		return "debit card payment done";
+	}
+	
+	public String CreditCardPayment()
+	{
+		return "credit card payment done";
+	}
 }
  
